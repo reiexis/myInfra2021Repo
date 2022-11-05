524a8fbe56fd9368f8f36375fbe28db0d77bd89b
reiexis <111543571+reiexis@users.noreply.github.com>
2022-11-05 12:07:53 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "mydev-tf-state-bucket0680"
     key = "main"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "my-dynamodb2-table"
   }
 }
